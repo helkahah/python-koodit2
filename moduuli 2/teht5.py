29399511fbea5355cf3780f28133335673256fa7
helkahah <helka.hahkio@metropolia.fi>
2023-01-13 10:58:10 +0200

    moduuli 2 tehtävät

--- moduuli 2/teht5.py
+++ moduuli 2/teht5.py
@@ -1,4 +1,18 @@
 import math
 
-luoti_str = (13.3)
-naula_str = (32)
+luoti_mää = input('Anna luotien määrä: ')
+naula_mää = input('Anna naulojen määrä: ')
+leiviskä_mää = input('Anna leivisköjen määrä: ')
+
+luoti = 13.3
+naula = 32*luoti
+leiviskä = 20*naula
+
+luoti_str = float(luoti_mää)
+naula_str = float(naula_mää)
+leiviskä_str = float(leiviskä_mää)
+
+massa_gr = (leiviskä*leiviskä_str)+(naula*naula_str)+(luoti*luoti_str)
+massa_kg = math.floor(massa_gr/1000)
+
+print(f'Massa nykymittojen mukaan: {massa_kg:3.0f} kilogrammaa ja ' + str(round(massa_gr-massa_kg*1000, 2)) + ' grammaa.')
