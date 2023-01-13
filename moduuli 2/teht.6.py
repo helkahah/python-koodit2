3d9d57f606c9f97d9e4eb93e11cc3ff694dd3b0d
helkahah <helka.hahkio@metropolia.fi>
2023-01-13 11:12:42 +0200

    moduuli 2 tehtävät

--- moduuli 2/teht.6.py
+++ moduuli 2/teht.6.py
@@ -2,7 +2,7 @@
 import random
 
 print('Arvioin kaksi numerolukon koodia:')
-koodi1 = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9)]
+koodi1 = random.randint(0, 9), random.randint(0, 9), random.randint(0, 9)
 print(koodi1)
-koodi2 = [random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), random.randint(1, 6)]
+koodi2 = random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), random.randint(1, 6)
 print(koodi2)
