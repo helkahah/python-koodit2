b97912fd7894cf182b4de77245b51c36f75b8652
helkahah <helka.hahkio@metropolia.fi>
2023-01-16 14:59:06 +0200

    moduuli 3 tehtävät

--- moduuli 2/teht5.py
+++ moduuli 2/teht5.py
@@ -1,18 +1,18 @@
 import math
 
-luoti_mää = input('Anna luotien määrä: ')
-naula_mää = input('Anna naulojen määrä: ')
-leiviskä_mää = input('Anna leivisköjen määrä: ')
+luoti_mää = float(input('Anna luotien määrä: '))
+naula_mää = int(input('Anna naulojen määrä: '))
+leiviskä_mää = int(input('Anna leivisköjen määrä: '))
 
 luoti = 13.3
 naula = 32*luoti
 leiviskä = 20*naula
 
-luoti_str = float(luoti_mää)
-naula_str = float(naula_mää)
-leiviskä_str = float(leiviskä_mää)
+#luoti_str = float(luoti_mää)
+#naula_str = float(naula_mää)
+#leiviskä_str = float(leiviskä_mää)
 
-massa_gr = (leiviskä*leiviskä_str)+(naula*naula_str)+(luoti*luoti_str)
-massa_kg = math.floor(massa_gr/1000)
+massa_gr = (leiviskä*leiviskä_mää)+(naula*naula_mää)+(luoti*luoti_mää)
+massa_kg = massa_gr//1000#math.floor(massa_gr/1000)
 
 print(f'Massa nykymittojen mukaan: {massa_kg:3.0f} kilogrammaa ja ' + str(round(massa_gr-massa_kg*1000, 2)) + ' grammaa.')
---
+++ moduuli 3/teht4.py
@@ -0,0 +1,8 @@
+import math
+
+vuosi = int(input('Anna vuosi: '))
+
+if vuosi % 4 == 0 and (vuosi % 100 != 0 or vuosi % 400 == 0):
+    print('Vuosi on karkausvuosi')
+else:
+    print('Vuosi ei ole karkausvuosi')
