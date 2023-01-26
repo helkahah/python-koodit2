d21a8e273c4f423754cf8b629edf147a1f27ac14
helkahah <helka.hahkio@metropolia.fi>
2023-01-26 14:37:57 +0200

    moduuli 5, teht 4

---
+++ moduuli 5/teht4.py
@@ -0,0 +1,9 @@
+# moduuli 5 tehtävä 4
+
+kaupungit = []
+for kaupunki in range(5):
+    kaupunki = input('Anna Kaupungin nimi:')
+    kaupungit.append(kaupunki)
+
+for i in range(5):
+    print(kaupungit[i])
