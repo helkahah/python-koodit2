eb15e5ff5cef84650da9d438a1a183e384d703cf
helkahah <helka.hahkio@metropolia.fi>
2023-03-21 11:19:27 +0200

    moduuli 11 tehty

---
+++ moduuli 11/teht1.py
@@ -0,0 +1,31 @@
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjoittaja, sivumäärä):
+        super().__init__(nimi)
+        self.kirjoittaja = kirjoittaja
+        self.sivumäärä = sivumäärä
+
+    def tulosta_tiedot(self):
+        print("Kirjan tiedot")
+        print(f"Nimi: {self.nimi}, kirjoittaja: {self.kirjoittaja}, "
+            f"sivuja: {self.sivumäärä}.")
+        return
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, päätoimittaja):
+        self.päätoimittaja = päätoimittaja
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        print("Lehden tiedot")
+        print(f"Nimi: {self.nimi}, Päätoimittaja: {self.päätoimittaja}")
+
+
+
+lehti = Lehti("Aku Ankka", "Aki Hyyppä")
+kirja = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+lehti.tulosta_tiedot()
+kirja.tulosta_tiedot()
---
+++ moduuli 11/teht2.py
@@ -0,0 +1,44 @@
+import random
+class Auto:
+    def __init__(self, rekkari, hnopeus):
+        print(f'Uusi auto {rekkari}, luotu')
+        self.rekkari = rekkari
+        self.hnopeus = hnopeus
+        self.nopeus = 0
+        self.matka = 0
+
+    def kiihdytä(self):
+        muutos = random.randrange(-10, 15)
+        self.nopeus += muutos
+        if self.nopeus<0:
+            self.nopeus -= self.nopeus
+        elif self.nopeus>self.hnopeus:
+            self.nopeus = self.hnopeus
+        else:
+            return
+
+    def kulje(self):
+        self.matka += self.nopeus
+
+class Sähköauto(Auto):
+    def __init__(self, rekkari, hnopeus, akkukapasiteetti):
+        self.akkukapasiteetti = akkukapasiteetti
+        super().__init__(rekkari, hnopeus)
+
+class Polttomottoriauto(Auto):
+    def __init__(self,rekkari, hnopeus, bensatankki):
+        self.bensatankki= bensatankki
+        super().__init__(rekkari, hnopeus)
+
+
+#pääohjelma
+sauto = Sähköauto("ABC-15", 180, 52.5)
+pauto = Polttomottoriauto("ABC-123", 165, 32.3)
+sauto.nopeus = 120
+pauto.nopeus = 130
+for i in range(3):
+    sauto.kulje()
+    pauto.kulje()
+
+print(f"Pauto kulki kolmessa tunnissa {pauto.matka}km, nopeus mittarissa oli {pauto.nopeus}km/h")
+print(f"Sauto kulki kolmessa tunnissa {sauto.matka}km, nopeus mittarissa oli {sauto.nopeus}km/h")
